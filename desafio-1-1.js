f715e004660bcc7966f46678a3187c2678f8198a
Stenio Oliveira <steniooliv@gmail.com>
2020-04-07 08:54:57 -0300

    update desafio1-1

--- desafio-1-1.js
+++ desafio-1-1.js
@@ -8,8 +8,10 @@
     - https://github.com/steniooliv/launchbase
 
 */
-
+//
 // CALCULO DE IMC
+//
+
 console.log("Calculo de IMC:")
 
 
@@ -24,11 +26,42 @@ const imc = peso / (altura * altura);
 if ( imc >= 30 ) {
 
   console.log(`${nome} você está acima do peso.`)
-
 }
 
 else {
 
   console.log(`${nome} você não está acima do peso.`)
+}
 
+//
+// CALCULO DE APOSENTADORIA (CALCULO FICTÍCIO)
+//
+
+console.log("Calculo de aposentadoria:")
+
+const nome2 = "Stenio";
+const sexo = "M";
+const idade = 29;
+const contribuicao = 13;
+
+// Regra fictícia para aposentadoria
+const regra = idade + contribuicao;
+
+
+if (sexo == "M" && contribuicao >= 35 || sexo == "F" && contribuicao >= 30) {
+  
+  if (sexo == "M" && regra >= 95 || sexo == "F" && regra >= 85) {
+
+    console.log(`${nome}, você pode se aposentar!`)
+  }
+  
+  else {
+
+    console.log(`${nome}, você ainda não pode se aposentar!`)
+  }
+}
+
+else {
+
+  console.log(`${nome}, você ainda não pode se aposentar!`)
 }
