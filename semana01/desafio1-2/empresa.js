f23b2dee1015c175cd3edfafe77bd8fae7930e53
Stenio Oliveira <steniooliv@gmail.com>
2020-04-07 11:20:54 -0300

    update desafio1-2

---
+++ semana01/desafio1-2/empresa.js
@@ -0,0 +1,15 @@
+//
+//  PROGRAMA PARA ARMAZENAR DADOS DA EMPRESA EM OBJETO
+//
+
+const empresa = {
+  nome: "Rocketseat",
+  cor: "Roxo",
+  foco: "Programação",
+  endereço: {
+    rua: "Rua Guilherme Gembala",
+    numero: 260,
+  }
+}
+
+console.log(`A empresa ${empresa.nome} está localizada em ${empresa.endereço.rua}, ${empresa.endereço.numero}`);
