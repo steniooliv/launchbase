94f63bd103523f1997710fe2e17401b717aac300
Stenio Oliveira <steniooliv@gmail.com>
2020-04-07 09:04:39 -0300

    update desafio 1-1 files

--- semana01/desafio1-1/aposentadoria.js
+++ semana01/desafio1-1/aposentadoria.js
@@ -1,38 +1,3 @@
-/*
-    DESAFIO 1-1
-
-    - CALCULO DE IMC
-
-    - CALCULO DE APOSENTADORIA
-
-    - https://github.com/steniooliv/launchbase
-
-*/
-//
-// CALCULO DE IMC
-//
-
-console.log("Calculo de IMC:")
-
-
-const nome = "Stenio";
-const peso = 60;
-const altura = 1.65;
-
-// Calculo do imc
-const imc = peso / (altura * altura);
-
-
-if (imc >= 30) {
-
-  console.log(`${nome} você está acima do peso.`)
-}
-
-else {
-
-  console.log(`${nome} você não está acima do peso.`)
-}
-
 //
 // CALCULO DE APOSENTADORIA (CALCULO FICTÍCIO)
 //
---
+++ semana01/desafio1-1/imc.js
@@ -0,0 +1,24 @@
+//
+// CALCULO DE IMC
+//
+
+console.log("Calculo de IMC:")
+
+
+const nome = "Stenio";
+const peso = 60;
+const altura = 1.65;
+
+// Calculo do imc
+const imc = peso / (altura * altura);
+
+
+if (imc >= 30) {
+
+  console.log(`${nome} você está acima do peso.`)
+}
+
+else {
+
+  console.log(`${nome} você não está acima do peso.`)
+}
