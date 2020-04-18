b8b76a1a19fd7ef4391c8e3fbc9c42c009fdc53c
Stenio Oliveira <steniooliv@gmail.com>
2020-04-17 23:55:55 -0300

    update backend to frontend

---
+++ semana01/backend/data.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
+    duration: "57 min",
+    price: "Free"
+  },
+
+  {
+    id: "2alg7MQ6_sI",
+    title: "Git e Github na Prática | Mayk Brito",
+    duration: "33 min",
+    price: "Free"
+  },
+
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a Variável THIS | Mayk Brito",
+    duration: "48 min",
+    price: "Free"
+  },
+
+  {
+    id: "HN1UjzRSdBk",
+    title: "Desvendando o CSS Grid na Prática | Mayk Brito",
+    duration: "36 min",
+    price: "Free"
+  },
+
+  {
+    id: "ghTrp1x_1As",
+    title: "O Que É API? REST e RESTFUL? | Mayk Brito",
+    duration: "33 min",
+    price: "Free"
+  },
+
+  {
+    id: "rAzHvYnQ8DY",
+    title: "ARRAY: High Order Functions | Mayk Brito",
+    duration: "54 min",
+    price: "Free"
+  }
+]
--- semana01/backend/server.js
+++ semana01/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const videos = require("./data");
 
 const server = express();
 
@@ -9,14 +10,28 @@ server.set("view engine","njk");
 
 nunjucks.configure("views", {
   express: server,
+  autoescape: false,
+  noCache: true,
 })
 
 server.get("/", function(req, res) {
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars0.githubusercontent.com/u/21336806?s=460&u=e62c6c92cd990a15a2c46213bf6868f58401b685&v=4",
+    name: "Stenio Oliveira",
+    role: "Aluno Desenvolvedor WEB",
+    description: 'Aluno de programção full-stack, focado em conhecer o melhor ensino para iniciantes em programação. Aluno da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+    links: [
+      { name: "Github", url: "https://www.github.com/steniooliv"},
+      { name: "Twitter", url: "https://www.twitter.com/steniooliv"},
+      { name: "Linkedin", url: "https://www.linkedin.com/in/steniooliv"},
+    ]
+  }
+
+  return res.render("about", {about});
 })
 
 server.get("/portfolio", function(req, res) {
-  return res.render("portfolio")
+  return res.render("portfolio", { items: videos });
 })
 
 server.listen(5000, function() {
