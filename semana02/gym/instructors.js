c73c19ecce58bfe57382e15d40a1f9f46d4154cf
Stenio Oliveira <steniooliv@gmail.com>
2020-05-12 22:25:02 -0300

    improve how to save data in data.json

--- semana02/gym/instructors.js
+++ semana02/gym/instructors.js
@@ -4,6 +4,7 @@ const data = require("./data.json");
 //create
 
 exports.post = function(req, res) {
+  
   const keys = Object.keys(req.body);
   
   for (key of keys) {
@@ -12,13 +13,26 @@ exports.post = function(req, res) {
     }
   }
 
+  let { avatar_url, name, birth, gender, services} = req.body;
 
-  data.instructors.push(req.body)
+  birth = Date.parse(req.body.birth);
+  created_at = Date.now();
+  id = Number(data.instructors.length + 1);
+  
+  data.instructors.push({
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at,
+  });
 
   fs.writeFile("./data.json", JSON.stringify(data, null, 2), function(err){
     if (err) return res.send("Write file error!");
+    return res.redirect("/instructors");
   })
-  return res.redirect("/instructors");
 }
 
 // update
