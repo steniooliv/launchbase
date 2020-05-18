625231db856ec9fda5059ec3cbcd417d757a3966
Stenio Oliveira <steniooliv@gmail.com>
2020-05-17 23:31:03 -0300

    add show instructor page

--- semana02/gym/instructors.js
+++ semana02/gym/instructors.js
@@ -1,5 +1,30 @@
 const fs = require("fs");
+const Intl = require('intl');
 const data = require("./data.json");
+const {age} = require("./date")
+
+//show
+
+exports.show = function(req, res) {
+  const {id} = req.params;
+
+  const foundInstructor = data.instructors.find(function(instructor){
+    return instructor.id == id;
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found.");
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.split(","),
+    created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
+  }
+
+  return res.render("instructors/show", {instructor});
+
+}
+
 
 //create
 
