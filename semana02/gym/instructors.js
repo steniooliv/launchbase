23055d26d71111f8862b4fe8f9c020606b65bdcf
Stenio Oliveira <steniooliv@gmail.com>
2020-05-24 17:19:29 -0300

    update push and delete instructors

--- semana02/gym/instructors.js
+++ semana02/gym/instructors.js
@@ -79,4 +79,51 @@ exports.edit = function(req, res) {
   return res.render("instructors/edit", {instructor: instructor});
 }
 
+//put
+
+exports.put = function(req, res) {
+  const {id} = req.body;
+  let index = 0;
+
+  const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+    if (id == instructor.id) {
+      index = foundIndex;
+      return true;
+    };
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found.");
+
+  
+  const instructor = {
+    ...foundInstructor,
+    ...req.body,
+    birth: Date.parse(req.body.birth),
+  }
+
+  data.instructors[index] = instructor;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Write error!");
+    return res.redirect(`/instructors/${id}`);
+  })
+
+}
+
 // delete
+
+exports.delete = function(req, res) {
+  const {id} = req.body;
+
+  const filteredInstructors = data.instructors.filter(function(instructor) {
+    return instructor.id != id;
+  })
+
+  data.instructors = filteredInstructors;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Delete error!");
+    return res.redirect("/instructors");
+  })
+
+}
--- semana02/gym/routes.js
+++ semana02/gym/routes.js
@@ -18,7 +18,11 @@ routes.get("/instructors/:id", instructors.show);
 
 routes.get("/instructors/:id/edit", instructors.edit);
 
-routes.post("/instructors", instructors.post)
+routes.post("/instructors", instructors.post);
+
+routes.put("/instructors", instructors.put);
+
+routes.delete("/instructors", instructors.delete);
 
 routes.get("/members", function(req, res) {
   return res.render("members/index");
