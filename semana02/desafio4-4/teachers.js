d4e4faf7625699e2954c850d600f6eb5653e44ae
Stenio Oliveira <steniooliv@gmail.com>
2020-05-24 14:46:54 -0300

    update desafio 4-4

--- semana02/desafio4-4/teachers.js
+++ semana02/desafio4-4/teachers.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const data = require("./data.json");
+const {age, date, graduation} = require("./utils");
+const Intl = require("intl");
 
 //
 
@@ -32,6 +34,45 @@ exports.post = function(req, res) {
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
     if (err) return res.send("Write file error!")
-    return res.redirect("/teachers");
+    return res.redirect("teachers");
   })
+}
+
+exports.show = function(req, res) {
+
+  const {id} = req.params;
+  const foundTeacher = data.teachers.find(function(teacher){
+    return id == teacher.id;
+  })
+
+  if (!foundTeacher) return res.send("Teacher not found!")
+
+  const teacher = {
+    ...foundTeacher,
+    age: age(foundTeacher.birth),
+    courses: foundTeacher.courses.split(","),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeacher.created_at),
+
+  }
+
+  return res.render("teachers/show", {teacher} );
+}
+
+exports.edit = function(req, res) {
+
+  const {id} = req.params;
+  const foundTeacher = data.teachers.find(function(teacher){
+    return id == teacher.id;
+  })
+
+  if (!foundTeacher) return res.send("Teacher not found!")
+
+  const teacher = {
+    ...foundTeacher,
+    birth: date(foundTeacher.birth),
+    // grade: graduation(foundTeacher.grade),
+
+  }
+
+  return res.render("teachers/edit", {teacher});
 }
