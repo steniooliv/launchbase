fb2234cd16ba0677a98bf771432227c782db29e3
Stenio Oliveira <steniooliv@gmail.com>
2020-06-14 13:58:36 -0300

    create members files and route

--- semana02/gym/routes.js
+++ semana02/gym/routes.js
@@ -27,4 +27,18 @@ routes.get("/members", function(req, res) {
 })
 
 
+routes.get("/members/create", function(req, res) {
+  return res.render("members/create");
+})
+
+routes.get("/members/:id", members.show);
+
+routes.get("/members/:id/edit", members.edit);
+
+routes.post("/members", members.post);
+
+routes.put("/members", members.put);
+
+routes.delete("/members", members.delete);
+
 module.exports = routes;
