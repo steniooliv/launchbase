956541a9f8eb1485ab91d3b93ecd6cc75c62eb23
Stenio Oliveira <steniooliv@gmail.com>
2020-06-18 00:58:08 -0300

    create grade function

--- semana02/desafio4-7/utils.js
+++ semana02/desafio4-7/utils.js
@@ -21,20 +21,53 @@ module.exports = {
     const month = `0${date.getUTCMonth() + 1}`.slice(-2);
     const day = `0${date.getUTCDate()}`.slice(-2);
 
-    return `${year}-${month}-${day}`;
+    return {
+      day,
+      month,
+      year,
+      birthDay: `${day}/${month}`,
+      iso: `${year}-${month}-${day}`,
+    }
   },
 
-  graduation: function(grade) {
-    if (grade == "highSchool") {
-      grade = "Ensino Médio Completo";
+  graduation: function(graduation) {
+    if (graduation == "highSchool") {
+      graduation = "Ensino Médio Completo";
+    }
+    else if (graduation == "graduate") {
+      graduation = "Ensino Superior Completo";
+    }
+    else {
+      graduation = "Mestrado e Doutorado";
     }
 
-    else if (grade == "graduate") {
-      grade = "Ensino Superior Completo";
+    return graduation;
+  },
+
+  grade: function(grade) {
+    if (grade == "5th") {
+      grade = "5º Ano Fundamental";
     }
-    
+    else if (grade == "6th") {
+      grade = "6º Ano Fundamental";
+    }
+    else if (grade == "7th") {
+      grade = "7º Ano Fundamental";
+    }
+    else if (grade == "8th") {
+      grade = "8º Ano Fundamental";
+    }
+    else if (grade == "9th") {
+      grade = "9º Ano Fundamental";
+    }
+    else if (grade == "1st") {
+      grade = "1º Ano Ensino Médio";
+    }
+    else if (grade == "2nd") {
+      grade = "2º Ano Ensino Médio";
+    }
     else {
-      grade = "Mestrado e Doutorado";
+      grade = "3º Ano Ensino Médio";
     }
 
     return grade;
