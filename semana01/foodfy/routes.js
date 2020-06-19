1c281fbb21a38419be33c70e8d14f5780853a756
Stenio Oliveira <steniooliv@gmail.com>
2020-06-18 23:34:16 -0300

    create default routes for admin

---
+++ semana01/foodfy/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const main = require("./controllers/main");
+const recipes = require("./controllers/recipes");
+const routes = express.Router();
+
+routes.get("/", main.index);
+routes.get("/about", main.about);
+routes.get("/recipes", main.recipes);
+routes.get("/recipes/:index", main.recipesIndex);
+
+routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+// routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+// routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+
+// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+// routes.put("/admin/recipes", recipes.put); // Editar uma receita
+// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+
+module.exports = routes;
--- semana01/foodfy/server.js
+++ semana01/foodfy/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
-const data = require("./data");
+const routes = require("./routes");
 
 
 const server = express();
 server.use(express.static("public"));
 server.use(express.static("assets"));
+server.use(routes);
 
 server.set("view engine", "njk");
 
@@ -14,26 +15,6 @@ nunjucks.configure("views", {
   autoescape: false,
   noCache: true,
 });
-
-server.get("/", function(req, res) {
-  return res.render("index", {data});
-});
-
-server.get("/about", function(req, res) {
-  return res.render("about");
-});
-
-server.get("/recipes", function(req, res) {
-  return res.render("recipes", {data});
-});
-
-server.get("/recipes/:index", function (req, res) {
-  const recipes = data; // Array de receitas carregadas do data.js
-  const recipeIndex = req.params.index;
-  
-  return res.render("details", {recipes: recipes[recipeIndex]});
-});
-
 
 server.listen(5000, function() {
   console.log("server is running");
