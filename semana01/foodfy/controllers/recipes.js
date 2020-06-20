aa6718d17722df3bbbd62a728bd8d7d42126a940
Stenio Oliveira <steniooliv@gmail.com>
2020-06-20 00:39:28 -0300

    create show, create and edit routes

--- semana01/foodfy/controllers/recipes.js
+++ semana01/foodfy/controllers/recipes.js
@@ -1,3 +1,15 @@
 exports.index = function(req, res) {
   return res.render("admin/recipes/index");
 }
+
+exports.show = function(req, res) {
+  return res.render("admin/recipes/show");
+}
+
+exports.create = function(req, res) {
+  return res.render("admin/recipes/create");
+}
+
+exports.edit = function(req, res) {
+  return res.render("admin/recipes/edit");
+}
