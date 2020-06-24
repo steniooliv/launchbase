104490d96bf314e8731d5d6e179046eab2cd25e2
Stenio Oliveira <steniooliv@gmail.com>
2020-06-24 00:32:12 -0300

    update index page

--- semana01/foodfy/controllers/recipes.js
+++ semana01/foodfy/controllers/recipes.js
@@ -1,5 +1,8 @@
+const fs = require("fs");
+const data = require("../data.json");
+
 exports.index = function(req, res) {
-  return res.render("admin/recipes/index");
+  return res.render("admin/recipes/index", {recipes: data.recipes});
 }
 
 exports.show = function(req, res) {
