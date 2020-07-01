59d7760ef6cfdd4863d54f1f4967301f864ccc3b
Stenio Oliveira <steniooliv@gmail.com>
2020-06-30 21:54:35 -0300

    add recipes edit routes

--- semana01/foodfy/controllers/recipes.js
+++ semana01/foodfy/controllers/recipes.js
@@ -2,11 +2,32 @@ const fs = require("fs");
 const data = require("../data.json");
 
 exports.index = function(req, res) {
+  for (recipe of data.recipes) {
+    recipe.id = data.recipes.indexOf(recipe);
+  }
+
   return res.render("admin/recipes/index", {recipes: data.recipes});
 }
 
 exports.show = function(req, res) {
-  return res.render("admin/recipes/show");
+  
+  for (recipe of data.recipes) {
+    recipe.id = data.recipes.indexOf(recipe);
+  }
+
+  const {id} = req.params;
+  
+  const foundRecipe = data.recipes.find(function(recipes) {
+    return id == recipes.id;
+  });
+
+  if (!foundRecipe) return res.send("Recipe not found!");
+
+  const recipes = {
+    ...foundRecipe
+  }
+
+  return res.render("admin/recipes/show", {recipes});
 }
 
 exports.create = function(req, res) {
@@ -14,5 +35,43 @@ exports.create = function(req, res) {
 }
 
 exports.edit = function(req, res) {
-  return res.render("admin/recipes/edit");
+  for (recipe of data.recipes) {
+    recipe.id = data.recipes.indexOf(recipe);
+  }
+
+  const {id} = req.params;
+  
+  const foundRecipe = data.recipes.find(function(recipes) {
+    return id == recipes.id;
+  });
+
+  if (!foundRecipe) return res.send("Recipe not found!");
+
+  const recipes = {
+    ...foundRecipe,
+    information: Array(foundRecipe.information)
+  }
+  console.log(recipes.information)
+
+  return res.render("admin/recipes/edit", {recipes});
+}
+
+exports.post = function(req, res) {
+  let id = 0;
+  const lastRecipe = data.recipes[data.recipes.length - 1];
+
+  if (lastRecipe) {
+    id = lastRecipe.id + 1;
+  }
+  
+  data.recipes.push({
+    id,
+    ...req.body,
+  });
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if (err) return res.send("Write file error!");
+    return res.redirect("/admin/recipes");
+  })
+
 }
--- semana01/foodfy/routes.js
+++ semana01/foodfy/routes.js
@@ -10,10 +10,10 @@ routes.get("/recipes/:index", main.recipesIndex);
 
 routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
 routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-routes.get("/admin/recipes/show", recipes.show); // Exibir detalhes de uma receita
-routes.get("/admin/recipes/edit", recipes.edit); // Mostrar formulário de edição de receita
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
-// routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
 // routes.put("/admin/recipes", recipes.put); // Editar uma receita
 // routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
