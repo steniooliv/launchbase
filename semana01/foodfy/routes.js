ba452dc27a7c79338d6ff3c6d81034a9773846f5
Stenio Oliveira <steniooliv@gmail.com>
2020-08-14 20:18:48 -0300

    add method put and delete

--- semana01/foodfy/routes.js
+++ semana01/foodfy/routes.js
@@ -14,7 +14,7 @@ routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receit
 routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
 routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
-// routes.put("/admin/recipes", recipes.put); // Editar uma receita
-// routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
+routes.put("/admin/recipes", recipes.put); // Editar uma receita
+routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
 module.exports = routes;
