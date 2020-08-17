ae386d30fed9397f33bae050d030b9970517d600
Stenio Oliveira <steniooliv@gmail.com>
2020-08-16 22:15:39 -0300

    first commit for gym with sql database

---
+++ semana03/gym/controllers/instructors.js
@@ -0,0 +1,135 @@
+const fs = require("fs");
+const Intl = require('intl');
+const data = require("../data.json");
+const {age, date} = require("../utils");
+
+exports.index = function(req, res) {
+
+  for (let instructor of data.instructors) {
+    instructor.services = String(instructor.services);
+    instructor.services = instructor.services.split(",");
+  }
+
+  return res.render("instructors/index", {instructors: data.instructors});
+
+};
+
+exports.show = function(req, res) {
+  const {id} = req.params;
+
+  const foundInstructor = data.instructors.find(function(instructor){
+    return id == instructor.id;
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found.");
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    // services: foundInstructor.services.split(","),
+    created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
+  }
+
+  return res.render("instructors/show", {instructor});
+
+};
+
+exports.create = function(req, res) {
+  return res.render("instructors/create")
+};
+
+exports.post = function(req, res) {
+  
+  const keys = Object.keys(req.body);
+  
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.send("Please fill all fields!")
+    }
+  }
+
+  let {avatar, name, birth, gender, services} = req.body;
+
+  birth = Date.parse(req.body.birth);
+  created_at = Date.now();
+  id = Number(data.instructors.length + 1);
+  
+  data.instructors.push({
+    id,
+    avatar,
+    name,
+    birth,
+    gender,
+    services,
+    created_at,
+  });
+
+  fs.writeFile("./data.json", JSON.stringify(data, null, 2), function(err){
+    if (err) return res.send("Write file error!");
+    return res.redirect("/instructors");
+  })
+};
+
+exports.edit = function(req, res) {
+  const {id} = req.params;
+
+  const foundInstructor = data.instructors.find(function(instructor){
+    return id == instructor.id;
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found.");
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth).iso
+  }
+
+  return res.render("instructors/edit", {instructor: instructor});
+};
+
+exports.put = function(req, res) {
+  const {id} = req.body;
+  let index = 0;
+
+  const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+    if (id == instructor.id) {
+      index = foundIndex;
+      return true;
+    };
+  })
+
+  if (!foundInstructor) return res.send("Instructor not found.");
+
+  
+  const instructor = {
+    ...foundInstructor,
+    ...req.body,
+    birth: Date.parse(req.body.birth),
+    id: Number(req.body.id),
+    services: String(req.body.services).split(","),
+  }
+
+  data.instructors[index] = instructor;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Write error!");
+    return res.redirect(`/instructors/${id}`);
+  })
+
+};
+
+exports.delete = function(req, res) {
+  const {id} = req.body;
+
+  const filteredInstructors = data.instructors.filter(function(instructor) {
+    return instructor.id != id;
+  })
+
+  data.instructors = filteredInstructors;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send("Delete error!");
+    return res.redirect("/instructors");
+  })
+
+};
---
+++ semana03/gym/utils.js
@@ -0,0 +1,65 @@
+module.exports = {
+
+  age: function(timestamp) {
+    const today = new Date();
+    const birthDate = new Date(timestamp);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+    
+    if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
+      age = age - 1;
+    }
+
+    return age;
+  },
+
+  date: function(timestamp) {
+    const date = new Date(timestamp);
+
+    const year = date.getUTCFullYear();
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+    const day = `0${date.getUTCDate()}`.slice(-2);
+
+    return {
+      day,
+      month,
+      year,
+      birthDay: `${day}/${month}`,
+      iso: `${year}-${month}-${day}`,
+    };
+  },
+
+  blood: function(blood) {
+    let bloodType = "";
+
+    if (blood == "A1") {
+      return bloodType = "A+";
+    } else
+    if (blood == "A0") {
+      return bloodType = "A-";
+    } else
+    if (blood == "B1") {
+      return bloodType = "B+";
+    } else
+    if (blood == "B0") {
+      return bloodType = "B-";
+    } else
+    if (blood == "AB1") {
+      return bloodType = "AB+";
+    } else
+    if (blood == "AB0") {
+      return bloodType = "AB-";
+    } else
+    if (blood == "O1") {
+      return bloodType = "O+";
+    } else
+    {
+      return bloodType = "O-";
+    }
+
+    return false;
+
+  }
+  
+}
