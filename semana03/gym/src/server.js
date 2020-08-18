67223b83d85a542e2d6230f6be03b0ff7c8aae04
Stenio Oliveira <steniooliv@gmail.com>
2020-08-17 22:37:38 -0300

    replace nunjucks view path

--- semana03/gym/src/server.js
+++ semana03/gym/src/server.js
@@ -12,7 +12,7 @@ server.use(routes);
 
 server.set("view engine", "njk")
 
-nunjucks.configure("src/views", {
+nunjucks.configure("src/app/views", {
   express: server,
   autoescape: false,
   noCache: true,
