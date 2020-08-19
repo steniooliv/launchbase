45c3be440efbc9e224aa31147f8ed9b67bbe00ca
Stenio Oliveira <steniooliv@gmail.com>
2020-08-18 21:30:59 -0300

    split method to Instructor.js

--- semana03/gym/src/app/controllers/instructors.js
+++ semana03/gym/src/app/controllers/instructors.js
@@ -1,11 +1,10 @@
-const Intl = require('intl');
-const {age, date} = require("../../lib/utils");
+const Instructor = require('../models/Instructor');
 
 module.exports = {
   index(req, res) {
-
-    return res.render("instructors/index");
-
+    Instructor.all(function(instructors) {
+      return res.render("instructors/index", {instructors});
+    });
   },
 
   show(req, res) {
@@ -21,7 +20,6 @@ module.exports = {
   },
 
   post(req, res) {
-
     const keys = Object.keys(req.body);
   
     for (key of keys) {
@@ -30,10 +28,9 @@ module.exports = {
       }
     }
 
-    let {avatar, name, birth, gender, services} = req.body;
-
-    return
-
+    Instructor.post(req.body,function(instructor) {
+      return res.redirect(`/instructors/${instructor.id}`);
+    });
   },
 
   edit(req, res) {
