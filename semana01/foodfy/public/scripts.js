3a835c73dde585bc300b74ca2520d03202759175
Stenio Oliveira <steniooliv@gmail.com>
2020-08-18 22:12:24 -0300

    Foodfy admin (#7)
    * create layout template
    
    * create default routes for admin
    
    * add logo for admin page
    
    * create admin recipes index page
    
    * create db data.json
    
    * split admin css
    
    * add admin css
    
    * update index page
    
    * create show, create and edit routes
    
    * add form styles
    
    * add routes for create, show and edit pages
    
    * add script for add input field on form
    
    * commit create page
    
    * commit edit page
    
    * commit fields page
    
    * commit show page
    
    * update index page
    
    * upgrade data.js to data.json
    
    * edit nodemon extensions monitoring
    
    * update index page
    
    * create delete form
    
    * add browser-sync
    
    * add recipes edit routes
    
    * add recipes for test
    
    * change button style
    
    * update scripts
    
    * edit button edit link
    
    * change button style
    
    * add  urlencoded on server use
    
    * add script link
    
    * change form action link
    
    * change link on button visualizar
    
    * edit ingredients and preparation fields
    
    * add recipes for test
    
    * add scripts.js
    
    * change list name
    
    * change edit route
    
    * edit post
    
    * add recipe for test
    
    * add recipes for test
    
    * change post
    
    * change buttons class
    
    * organize forms and divs
    
    * edit inputs value
    
    * remove from foodfy-admin
    
    * add method put and delete
    
    * remove preview
    
    * edit and delete recipes
    
    * update admin routes
    
    * create put and delete functions
    
    * edit buttons class
    
    * create script to confirm delete
    
    * edit forms and add confirm to delete
    
    * insert create recipes button
    
    * first commit for gym with sql database
    
    * restructuring files and folders
    
    * delete from foodfy branch
    
    * delete from foodfy branch

--- semana01/foodfy/public/scripts.js
+++ semana01/foodfy/public/scripts.js
@@ -21,3 +21,36 @@ for (let i = 0; i < hide.length; i++) {
     }
   })
 }
+
+// add ingredient
+
+function addIngredient() {
+
+  const ingredients = document.querySelector(".ingredients");
+  const fieldContainer = document.querySelectorAll(".ingredient");
+
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  if (newField.children[0].value == "") return false;
+
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
+}
+
+document.querySelector(".add-ingredient").addEventListener("click",addIngredient);
+
+// add prepare
+
+function addPreparation() {
+  const preparations = document.querySelector(".preparations");
+  const fieldContainer = document.querySelectorAll(".preparation");
+
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  if (newField.children[0].value == "") return false;
+
+  newField.children[0].value = "";
+  preparations.appendChild(newField);
+}
+
+document.querySelector(".add-prepare").addEventListener("click",addPreparation);
--- semana01/foodfy/server.js
+++ semana01/foodfy/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
-const data = require("./data");
+const routes = require("./routes");
+const methodOverride = require("method-override");
 
 
 const server = express();
+server.use(express.urlencoded({extended: true}));
 server.use(express.static("public"));
 server.use(express.static("assets"));
+server.use(methodOverride("_method"));
+server.use(routes);
 
 server.set("view engine", "njk");
 
@@ -14,26 +18,6 @@ nunjucks.configure("views", {
   autoescape: false,
   noCache: true,
 });
-
-server.get("/", function(req, res) {
-  return res.render("index", {data});
-});
-
-server.get("/about", function(req, res) {
-  return res.render("about");
-});
-
-server.get("/recipes", function(req, res) {
-  return res.render("recipes", {data});
-});
-
-server.get("/recipes/:index", function (req, res) {
-  const recipes = data; // Array de receitas carregadas do data.js
-  const recipeIndex = req.params.index;
-  
-  return res.render("details", {recipes: recipes[recipeIndex]});
-});
-
 
 server.listen(5000, function() {
   console.log("server is running");
