27eca3b1b9d1bd9a1f3df7c01ab36cdd69ecbd21
Stenio Oliveira <steniooliv@gmail.com>
2020-08-27 22:41:38 -0300

    create crud for student

--- semana03/desafio5-2/src/app/controllers/students.js
+++ semana03/desafio5-2/src/app/controllers/students.js
@@ -1,8 +1,16 @@
-const {age, date, grade} = require("../../lib/utils");
+const { date, grade } = require("../../lib/utils");
+const Student = require('../models/Student');
 
 module.exports = {
   index(req, res) {
-    return res.render('students/index');
+    Student.all(function(student) {
+      
+      for (let st of student) {
+        st.grade = grade(st.grade);
+      }
+
+      return res.render('students/index', {students: student});
+    });
   },
 
   create(req, res) {
@@ -10,22 +18,48 @@ module.exports = {
   },
 
   show(req, res) {
-    return res.render('students/show');
+    Student.find(req.params.id, function(student) {
+      if (!student) return res.send('Student not found!');
+
+      student.birth = date(student.birth).birthDay;
+      student.grade = grade(student.grade);
+
+      return res.render('students/show', {student});
+    });
   },
 
   post(req, res) {
-    return
+    const keys = Object.keys(req.body);
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Please fill all fields!");
+      }
+    }
+
+    Student.create(req.body, function(student) {
+      return res.redirect(`students/${student.id}`);
+    });
   },
 
   edit(req, res) {
-    return res.render('students/edit');
+    Student.find(req.params.id, function(student) {
+      if (!student) return res.send('Student not found!');
+
+      student.birth = date(student.birth).iso;
+
+      return res.render('students/edit', {student});
+    });
   },
 
   update(req, res) {
-    return res.render('students/show');
+    Student.update(req.body, function() {
+      return res.redirect(`students/${req.body.id}`);
+    });
   },
 
   delete(req, res) {
-    return res.render('students/index');
+    Student.delete(req.body.id, function() {
+      return res.redirect('students');
+    });
   }
 }
