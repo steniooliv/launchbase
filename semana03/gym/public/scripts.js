238139262d47898f59c579731131608db4ce1eb6
Stenio Oliveira <steniooliv@gmail.com>
2020-09-06 22:54:10 -0300

    create function paginate

--- semana03/gym/public/scripts.js
+++ semana03/gym/public/scripts.js
@@ -6,3 +6,32 @@ if (currentPage.includes(item.getAttribute("href"))) {
     item.classList.add("active")
   }
 }
+
+function paginate(selectedPage, totalPages) {
+
+  let pages = [];
+  let oldPage = 0;
+
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+    const firstAndLastPage = currentPage == 1 || currentPage == totalpages;
+
+    if ( firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...");
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1);
+      }
+
+      pages.push(currentPage);
+      oldPage = currentPage;
+    }
+
+  }
+
+  return pages;
+}
