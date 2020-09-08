68710f894cfba5b96b3528d21af221e71111ee38
Stenio Oliveira <steniooliv@gmail.com>
2020-09-07 21:10:47 -0300

    add pagination script

--- semana03/gym/public/scripts.js
+++ semana03/gym/public/scripts.js
@@ -16,7 +16,7 @@ function paginate(selectedPage, totalPages) {
     
     const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
     const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
-    const firstAndLastPage = currentPage == 1 || currentPage == totalpages;
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
 
     if ( firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
       if (oldPage && currentPage - oldPage > 2) {
@@ -34,4 +34,36 @@ function paginate(selectedPage, totalPages) {
   }
 
   return pages;
+}
+
+function createPagination(pagination) {
+
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const filter = pagination.dataset.filter;
+
+const pages = paginate(page, total);
+
+
+let elements = "";
+
+for (let page of pages) {
+  if (String(page).includes("...")) {
+    elements += `<span>${page}</span>`
+  } else {
+    if (filter) {
+      elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`  
+    } else {
+      elements += `<a href="?page=${page}">${page}</a>`
+    }
+  }
+}
+
+pagination.innerHTML = elements;
+}
+
+const pagination = document.querySelector(".pagination");
+
+if (pagination) {
+  createPagination(pagination);
 }
