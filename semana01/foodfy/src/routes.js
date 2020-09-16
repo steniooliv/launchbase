7037aba046991f0c38b89563d423ce9f2111ac09
Stenio Oliveira <steniooliv@gmail.com>
2020-09-15 22:50:04 -0300

    create routes for admin chefs

--- semana01/foodfy/src/routes.js
+++ semana01/foodfy/src/routes.js
@@ -15,18 +15,22 @@ routes.get("/chefs", recipes.chefs);
 routes.get("/search", recipes.search);
 
 routes.get("/admin/recipes", adminRecipes.index);
-routes.get("/admin/recipes/create", adminRecipes.create);
-routes.get("/admin/recipes/:id", adminRecipes.show);
-routes.get("/admin/recipes/:id/edit", adminRecipes.edit);
+// routes.get("/admin/recipes/create", adminRecipes.create);
+// routes.get("/admin/recipes/:id", adminRecipes.show);
+// routes.get("/admin/recipes/:id/edit", adminRecipes.edit);
 
-routes.post("/admin/recipes", adminRecipes.post);
-routes.put("/admin/recipes", adminRecipes.put);
-routes.delete("/admin/recipes", adminRecipes.delete);
+// routes.post("/admin/recipes", adminRecipes.post);
+// routes.put("/admin/recipes", adminRecipes.put);
+// routes.delete("/admin/recipes", adminRecipes.delete);
 
 routes.get("/admin/chefs", adminChefs.index);
-// routes.get("/admin/chefs/create", adminChefs.create);
+routes.get("/admin/chefs/create", adminChefs.create);
 routes.get("/admin/chefs/:id", adminChefs.show);
-// routes.get("/admin/chefs/:id/edit", adminChefs.edit);
+routes.get("/admin/chefs/:id/edit", adminChefs.edit);
+
+routes.post("/admin/chefs", adminChefs.post);
+routes.put("/admin/chefs", adminChefs.put);
+routes.delete("/admin/chefs", adminChefs.delete);
 
 
 module.exports = routes;
