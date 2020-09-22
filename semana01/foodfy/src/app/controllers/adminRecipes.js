7ea4034c693308b168bb78c93a92857d67d5366a
Stenio Oliveira <steniooliv@gmail.com>
2020-09-22 00:46:11 -0300

    create post and find methods

--- semana01/foodfy/src/app/controllers/adminRecipes.js
+++ semana01/foodfy/src/app/controllers/adminRecipes.js
@@ -8,112 +8,33 @@ module.exports = {
     });
   },
 
-}
+  show(req, res) {
+    AdminRecipe.find(req.params.id, function(recipes) {
+      
+      return res.render("admin/recipes/show", {recipes});
+    })
+  },
+
+  create(req, res) {
+    AdminRecipe.chefSelectOption(function(options) {
+      
+      return res.render("admin/recipes/create", {chefOptions: options} );
+    });
+  },
+
+  post(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Fill all fields!");
+      }
+    }
+
+    AdminRecipe.post(req.body, function(recipe) {
+      return res.redirect(`/admin/recipes/${recipe.id}`);
+    });
+  }
 
 
-exports.show = function(req, res) {
-  
-  // for (recipe of data.recipes) {
-  //   recipe.id = data.recipes.indexOf(recipe);
-  // }
-
-  // const {id} = req.params;
-  
-  // const foundRecipe = data.recipes.find(function(recipes) {
-  //   return id == recipes.id;
-  // });
-
-  // if (!foundRecipe) return res.send("Recipe not found!");
-
-  // const recipes = {
-  //   ...foundRecipe
-  // }
-
-  return res.render("admin/recipes/show");
-}
-
-exports.create = function(req, res) {
-  return res.render("admin/recipes/create");
-}
-
-exports.edit = function(req, res) {
-  // for (recipe of data.recipes) {
-  //   recipe.id = data.recipes.indexOf(recipe);
-  // }
-
-  // const {id} = req.params;
-  
-  // const foundRecipe = data.recipes.find(function(recipes) {
-  //   return id == recipes.id;
-  // });
-
-  // if (!foundRecipe) return res.send("Recipe not found!");
-
-  // const recipes = {
-  //   ...foundRecipe,
-  // }
-
-  return res.render("admin/recipes/edit");
-}
-
-exports.post = function(req, res) {
-  // const keys = Object.keys(req.body);
-
-  // for (key of keys) {
-  //   if (req.body[key] == "") {
-  //     return res.send("Fill all fields!");
-  //   }
-  // }
-  
-  // data.recipes.push({
-  //   ...req.body,
-  // });
-
-  // fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-  //   if (err) return res.send("Write file error!");
-  // })
-  
-    return res.redirect("/admin/recipes");
-}
-
-exports.put = function(req, res) {
-  // const {id} = req.body;
-  // let index = 0;
-
-  // const foundRecipe = data.recipes.find(function(recipe, foundIndex) {
-  //   if (id == recipe.id) {
-  //     index = foundIndex;
-  //     return true;
-  //   }
-  // });
-
-  // if (!foundRecipe) return res.send("Recipe not found.");
-
-  // const recipe = {
-  //   ...foundRecipe,
-  //   ...req.body,
-  //   id: Number(req.body.id),
-  // }
-
-  // data.recipes[index] = recipe;
-
-  // fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-  //   if (err) return res.send("Write file error!");
-  // });
-    return res.redirect(`/admin/recipes/${id}`);
-}
-
-exports.delete = function(req, res) {
-  // const {id} = req.body;
-
-  // const filteredRecipes = data.recipes.filter(function(recipe) {
-  //   return recipe.id != id;
-  // });
-
-  // data.recipes = filteredRecipes;
-
-  // fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
-  //   if (err) return res.send("Delete file error!");
-  // });
-    return res.redirect("/admin/recipes/");
 }
--- semana01/foodfy/src/app/models/AdminRecipe.js
+++ semana01/foodfy/src/app/models/AdminRecipe.js
@@ -1,10 +1,12 @@
+const {date} = require("../../lib/utils");
 const db = require("../../config/db");
 
 module.exports = {
   all(callback) {
     const query = `
-      SELECT *
+      SELECT recipes.*, chefs.name AS author
       FROM recipes
+      LEFT JOIN chefs ON (chefs.id = recipes.chef_id)
       ORDER BY title ASC
     `
 
@@ -14,5 +16,73 @@ module.exports = {
       callback(results.rows);
     });
   },
+
+  find(id, callback) {
+    const query = `
+      SELECT recipes.*, chefs.name AS author
+      FROM recipes
+      LEFT JOIN chefs ON (chefs.id = recipes.chef_id)
+      WHERE recipes.id = $1
+    `
+
+    const values = [
+      id,
+    ]
+
+    db.query(query, values, function(err, results) {
+      if (err) throw `Database Error! ${err}`;
+
+      callback(results.rows[0]);
+    })
+  },
+
+  chefSelectOption(callback) {
+    const query = `
+      SELECT name, id
+      FROM chefs
+    `
+    db.query(query, function(err, results) {
+      if (err) throw `Database Error! ${err}`;
+
+      callback(results.rows);
+    })
+  },
+
+  post(data, callback) {
+    const query = `
+      INSERT INTO recipes (
+        chef_id,
+        image,
+        title,
+        ingredients,
+        preparation,
+        information,
+        created_at
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING id
+    `
+    const values = [
+      data.chef_id,
+      data.image,
+      data.title,
+      data.ingredients,
+      data.preparation,
+      data.information,
+      date(Date.now()).format,
+    ]
+
+    console.log(data.ingredients);
+    console.log(data.preparation);
+
+    db.query(query, values, function(err, results) {
+      if (err) throw `Database Error! ${err}`;
+
+      callback(results.rows[0]);
+    })
+  }
+
+
+
+
 
 }
