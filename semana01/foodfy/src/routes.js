ce4932b42a0c4b39b394c543adc22c9884e393ab
Stenio Oliveira <steniooliv@gmail.com>
2020-09-22 00:44:50 -0300

    create new routes

--- semana01/foodfy/src/routes.js
+++ semana01/foodfy/src/routes.js
@@ -15,11 +15,11 @@ routes.get("/chefs", recipes.chefs);
 routes.get("/search", recipes.search);
 
 routes.get("/admin/recipes", adminRecipes.index);
-// routes.get("/admin/recipes/create", adminRecipes.create);
-// routes.get("/admin/recipes/:id", adminRecipes.show);
+routes.get("/admin/recipes/create", adminRecipes.create);
+routes.get("/admin/recipes/:id", adminRecipes.show);
 // routes.get("/admin/recipes/:id/edit", adminRecipes.edit);
 
-// routes.post("/admin/recipes", adminRecipes.post);
+routes.post("/admin/recipes", adminRecipes.post);
 // routes.put("/admin/recipes", adminRecipes.put);
 // routes.delete("/admin/recipes", adminRecipes.delete);
 
