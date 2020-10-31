0434e1220451b049b67e1d5a8b75bcc68b8c2561
Stenio Oliveira <steniooliv@gmail.com>
2020-10-31 19:53:10 -0300

    create edti and put routes

--- semana01/foodfy/src/routes.js
+++ semana01/foodfy/src/routes.js
@@ -17,10 +17,10 @@ routes.get("/search", recipes.search);
 routes.get("/admin/recipes", adminRecipes.index);
 routes.get("/admin/recipes/create", adminRecipes.create);
 routes.get("/admin/recipes/:id", adminRecipes.show);
-// routes.get("/admin/recipes/:id/edit", adminRecipes.edit);
+routes.get("/admin/recipes/:id/edit", adminRecipes.edit);
 
 routes.post("/admin/recipes", adminRecipes.post);
-// routes.put("/admin/recipes", adminRecipes.put);
+routes.put("/admin/recipes", adminRecipes.put);
 // routes.delete("/admin/recipes", adminRecipes.delete);
 
 routes.get("/admin/chefs", adminChefs.index);
